366f50da1eb325373d89277ed4815a5f1a3d70e7
James Abney <james.abney@gmail.com>
2018-09-20 22:30:33 -0700

    Add token.model.js

---
+++ models/token.model.js
@@ -0,0 +1,117 @@
+const helpers = require('../lib/helpers')
+const Model = require('./model')
+
+/**
+ * @typedef {Object} TokenRecord
+ * @property {string} id
+ * @property {string} userId
+ * @property {number} expires
+ */
+
+class Token extends Model {
+  /**
+   * @param {string} [id]
+   */
+  constructor(id) {
+    super('tokens', id)
+
+    /**
+     * @type {string}
+     */
+    this.id = null
+
+    /**
+     * @type {string}
+     */
+    this.userId = null
+
+    /**
+     * @type {number} Epoch Time
+     */
+    this.expires = null
+  }
+
+  /**
+   * Return members of this model as elements of an object.
+   *
+   * @returns {TokenRecord}
+   */
+  serialize() {
+    const serializedToken = {
+      id: this.id,
+      userId: this.userId,
+      expires: this.expires,
+    }
+
+    return serializedToken
+  }
+
+  /**
+   * Assign elements from given data to members of this model.
+   *
+   * @param {TokenRecord} data
+   */
+  deserialize(data) {
+    this.id = typeof data.id === 'undefined' && this.id || data.id
+    this.userId = typeof data.userId === 'undefined' && this.userId || data.userId
+    this.expires = typeof data.expires === 'undefined' && this.expires || data.expires
+    return this
+  }
+
+  /**
+   * Extend this token by the given ms.
+   *
+   * @param {number} ms The number of milliseconds to extend the token.
+   */
+  extend(ms) {
+    this.expires = Date.now() + ms
+    return this
+  }
+
+  /**
+   * Verify that a token is valid -- that it exists and is not expired.
+   */
+  verify() {
+    if (typeof this.id === 'undefined') {
+      return false
+    }
+
+    return Token.verify(this.id)
+  }
+}
+
+/**
+ * Verify that a token is valid -- that it exists and is not expired.
+ *
+ * @param {string} tokenId
+ */
+Token.verify = async function verify(tokenId) {
+  try {
+    const token = new Token(tokenId)
+    await token.load()
+    return token.expires > Date.now()
+  } catch (error) {
+    return false
+  }
+}
+
+/**
+ * Create a new token.
+ *
+ * @param {string} userId
+ * @param {number} validForMs
+ */
+Token.create = async function create(userId, validForMs) {
+  const id = helpers.createRandomString(32)
+  const token = new Token(id)
+  const data = { id, userId, expires: Date.now() + validForMs }
+  token.deserialize(data)
+  await token.save()
+  return token
+}
+
+Token.delete = async function del(tokenId) {
+  new Token(tokenId).delete()
+}
+
+module.exports = Token
